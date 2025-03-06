175f2a081378fc9c22b8c37137b7f867911387a3
Davydovan <davydovan06@gmail.com>
2025-03-06 02:54:42 +0200

    changes for packages

--- UIProject/playwright.config.ts
+++ UIProject/playwright.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   timeout: 50000,
   testDir: './tests',
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
